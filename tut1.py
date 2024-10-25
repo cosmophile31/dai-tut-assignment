c281e0bbaf7b73b77f08d94ed555dbeff28a206b
cosmophile31 <snehankitabhagat@gmail.com>
2024-10-26 00:15:25 +0530

    Add files via upload

---
+++ tut1.py
@@ -0,0 +1,24 @@
+name = input("what is your name? ")
+
+print( " Hi " + name)
+
+s = "snehankita"
+print(s[0:-5])
+
+
+a = 10
+b = 5
+
+print(a>b)
+
+
+mylist = ["cherry" , "peach" , "banana" ]
+
+for x in mylist:
+    print(x)
+
+
+list1 = [1, 2, 3, [4,5]]
+print(list1)
+print(len(list1))
+
